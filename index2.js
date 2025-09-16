5ecd4d949c541b037eef28b9bd565ef5f80567a0
Akshara Reddy <aksharavooturi25@gmail.com>
2025-09-16 15:44:47 +0530

    fetching users

--- index2.js
+++ index2.js
@@ -1,6 +1,25 @@
-const form= document.getElementById('contactus');
- form.addEventListener('submit', function(e) {
-      e.preventDefault(); 
-      alert("Thank you! Your message has been sent.");
-      form.reset();
- });
+async function fetchUser() {
+     const num=document.getElementById("userId").value;
+     const output=document.getElementById("output")
+     try{
+          const res=await fetch(`https://fakestoreapi.com/users/${num}`);
+          const data=await res.json();
+           if (!data.id) {
+            throw new Error("User not exist");
+          }
+          output.innerHTML= `
+            <div class="user-card">
+            <h2>${data.name.firstname} ${data.name.lastname}</h2>
+            <p><strong>Email:</strong> ${data.email}</p>
+                <p><strong>Username:</strong> ${data.username}</p>
+                <p><strong>Phone:</strong> ${data.phone}</p>
+                <p><strong>City:</strong> ${data.address.city}</p>
+            </div>
+          `
+          console.log(data);
+
+     }
+     catch(error){
+          output.innerHTML= `<div class="error">⚠️ user not exists</div>`
+     }
+}
